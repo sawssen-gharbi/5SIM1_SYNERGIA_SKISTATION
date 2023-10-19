5f900acbe3d930f19bec3582f4b403b5c6049cdb
Sawssen Gharbi <sawssen.gharbi@esprit.tn>
2023-10-19 19:40:22 +0100

    Mockito Instructor

--- src/main/java/tn/esprit/spring/services/InstructorServicesImpl.java
+++ src/main/java/tn/esprit/spring/services/InstructorServicesImpl.java
@@ -1,6 +1,8 @@
 package tn.esprit.spring.services;
 
 import lombok.AllArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Service;
 import tn.esprit.spring.entities.Course;
 import tn.esprit.spring.entities.Instructor;
@@ -19,26 +21,36 @@ public class InstructorServicesImpl implements IInstructorServices{
     private ICourseRepository courseRepository;
 
     @Override
+    @Test
+    @Order(0)
     public Instructor addInstructor(Instructor instructor) {
         return instructorRepository.save(instructor);
     }
 
     @Override
+    @Test
+    @Order(2)
     public List<Instructor> retrieveAllInstructors() {
         return instructorRepository.findAll();
     }
 
     @Override
+    @Test
+    @Order(1)
     public Instructor updateInstructor(Instructor instructor) {
         return instructorRepository.save(instructor);
     }
 
     @Override
+    @Test
+    @Order(3)
     public Instructor retrieveInstructor(Long numInstructor) {
         return instructorRepository.findById(numInstructor).orElse(null);
     }
 
     @Override
+    @Test
+    @Order(4)
     public Instructor addInstructorAndAssignToCourse(Instructor instructor, Long numCourse) {
         Course course = courseRepository.findById(numCourse).orElse(null);
         Set<Course> courseSet = new HashSet<>();
--- src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
+++ src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
@@ -1,13 +1,28 @@
 package tn.esprit.spring;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import tn.esprit.spring.repositories.IInstructorRepository;
+import tn.esprit.spring.services.InstructorServicesImpl;
 
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
+@ExtendWith(MockitoExtension.class)
 class GestionStationSkiApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+
+	@Mock
+	IInstructorRepository instructorRepository;
+
+	@InjectMocks
+	InstructorServicesImpl instructorServices;
 
 }
